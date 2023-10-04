889570a4bf39045b53de02db6e0982977254d882
Nihal <115047952+N-i-h-al@users.noreply.github.com>
2023-10-04 23:40:20 +0600

    Change houa ki nahi

--- travelproject/settings.py
+++ travelproject/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = "django-insecure-n3d8%nvvo_x-zu%dp*231@r310_8-3*ejofr6z&bfhdh7)^w3d
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1','shaifuldjango.pythonanywhere.com']
 
 
 # Application definition
